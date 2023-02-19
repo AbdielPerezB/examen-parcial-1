de2e2c439a18f36a2ea5f8caef5ecee3687c3d77
Abdiel <abdiel2012p@gmail.com>
2023-02-18 18:53:16 -0600

    A) Put listo

--- routers/A_CRUD_Continentes.py
+++ routers/A_CRUD_Continentes.py
@@ -21,11 +21,11 @@ routerContinentes = APIRouter()
 
 #Get
 @routerContinentes.get("/continent/", status_code=status.HTTP_200_OK)
-async def imprimirContinentes():
+async def read():
     return continentes_lista
 
 @routerContinentes.get("/continent/{id}", status_code=status.HTTP_200_OK)
-async def imprimirContinentes(id: int):
+async def read(id: int):
     continentes = filter(lambda continente: continente.id == id, continentes_lista)
     try:
         return list(continentes)[0]
@@ -33,11 +33,27 @@ async def imprimirContinentes(id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
 #Post
-@routerContinentes.post("/continent/")
-async def usersclass(continente:Continente):
+@routerContinentes.post("/continent/", response_model=Continente, status_code=status.HTTP_201_CREATED)
+async def create(continente:Continente):
     for aux in continentes_lista:
         if aux.id == continente.id:  #Si el Id de los usuarios en la lista es igual al Id del usuario nuevo
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="El continente ya existe")
     else:
         continentes_lista.append(continente)
+        return continente
+
+#Put
+@routerContinentes.put("/continent/", response_model=Continente, status_code=status.HTTP_201_CREATED)
+async def update(continente: Continente):
+    
+    found=False
+    
+    for index, saved in enumerate(continentes_lista):
+        if saved.id == continente.id:
+           continentes_lista[index] = continente  #accedemos al indice de la lista que hemos encontrado y actualizamos con el nuevo usuario
+           found=True
+           
+    if not found:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    else:
         return continente
