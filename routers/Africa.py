83f67a53418accbb5191f265e71a6194f7ea86f5
LuisGoav00 <101147526+LuisGoav00@users.noreply.github.com>
2023-02-20 21:54:58 -0600

    Limpiamos código

--- routers/Africa.py
+++ routers/Africa.py
@@ -20,8 +20,6 @@ regiones_lista=[
 routerRegiones = APIRouter()
 
  
-    #code,name,continent,region,surface_area,independence_year,population,life_expectancy,
-#gnp,gnp_old,local_name,government_form,head_of_state,capital,code2
 
 #Función Get:
 @routerRegiones.get("/africa/",status_code=status.HTTP_200_OK)
@@ -41,7 +39,6 @@ async def africa(id: int):
 @routerRegiones.post("/africa/", response_model=Regiones, status_code=status.HTTP_201_CREATED)
 async def africa(region:Regiones):
     
-    found=False     #Usamos bandera found para verificar si hemos encontrado el usuario 
     
     for saved_regiones in regiones_lista:
         if saved_regiones.Id == region.Id:  #Si el Id del usuario guardado es igual al Id del usuario nuevo
@@ -50,7 +47,6 @@ async def africa(region:Regiones):
         regiones_lista.append(region)
         return region
     
-    #http://127.0.0.1:8000/usersclass/
 
     #***Put (update). Es decir, de un usuario que YA EXISTE, lo va a modificar
 @routerRegiones.put("/africa/", response_model=Regiones, status_code=status.HTTP_201_CREATED)
