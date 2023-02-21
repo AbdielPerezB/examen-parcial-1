f99c88575acb2c217baaff54f95d342c5ecc9471
LuisGoav00 <101147526+LuisGoav00@users.noreply.github.com>
2023-02-20 21:58:16 -0600

    Limpiamos código

--- routers/North_America.py
+++ routers/North_America.py
@@ -18,9 +18,6 @@ regiones_lista=[
 routerRegiones = APIRouter()
 
  
-    #code,name,continent,region,surface_area,independence_year,population,life_expectancy,
-#gnp,gnp_old,local_name,government_form,head_of_state,capital,code2
-
 #Función Get:
 @routerRegiones.get("/north_america/",status_code=status.HTTP_200_OK)
 async def north_america():
@@ -39,7 +36,6 @@ async def north_america(id: int):
 @routerRegiones.post("/north_america/", response_model=Regiones, status_code=status.HTTP_201_CREATED)
 async def north_america(region:Regiones):
     
-    found=False     #Usamos bandera found para verificar si hemos encontrado el usuario 
     
     for saved_regiones in regiones_lista:
         if saved_regiones.Id == region.Id:  #Si el Id del usuario guardado es igual al Id del usuario nuevo
@@ -48,7 +44,6 @@ async def north_america(region:Regiones):
         regiones_lista.append(region)
         return region
     
-    #http://127.0.0.1:8000/usersclass/
 
     #***Put (update). Es decir, de un usuario que YA EXISTE, lo va a modificar
 @routerRegiones.put("/north_america/", response_model=Regiones, status_code=status.HTTP_201_CREATED)
@@ -66,9 +61,7 @@ async def north_america(region:Regiones):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     else:
         return region
-    
-    #http://127.0.0.1:8000/usersclass/
-    
+        
     
         #***Delete
 @routerRegiones.delete("/north_america/{id}", status_code=status.HTTP_204_NO_CONTENT) #Aquí no es necesario poner todo el usuario, con el id basta para eoncontrarlo y eliminarlo
